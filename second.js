f8784c9db0566ff1e6bef5c5e72e7d135fae482c
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-03-08 07:02:58 +0500

    Add files via upload

---
+++ second.js
@@ -0,0 +1,10 @@
+//(function(exports,require,module,__filename,__dirname){
+
+value={
+    name:"Jack",
+    age:26,
+    developer:true
+}
+console.log(exports,require,module,__filename,__dirname);
+module.exports=value;
+//})
