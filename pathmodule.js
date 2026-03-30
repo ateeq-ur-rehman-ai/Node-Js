eb3421b37114ffdfd110e48e0d3c764c13f5a92d
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-03-30 18:24:24 +0500

    Add files via upload

---
+++ pathmodule.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const a=path.basename('C:\\temp\\myfile.html');
+console.log(a)
+
+const b=path.dirname('C:\\temp\\myfile.html');
+console.log(b)
+
+const c=path.extname(__filename);
+console.log(__filename,c)
+
